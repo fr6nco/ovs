6fd23d89b85f3c169e946f10b845140df7291d47
Thomas Boros <thomas@Thomass-MacBook-Pro.local>
2017-11-03 17:31:59 +0100

    bad declaration

--- lib/packets.h
+++ lib/packets.h
@@ -436,7 +436,7 @@ void pop_eth(struct dp_packet *packet);
 
 void inc_seq(struct dp_packet *packet);
 
-void inc_ack(struct db_packet *packet);
+void inc_ack(struct dp_packet *packet);
 
 void encap_nsh(struct dp_packet *packet,
                const struct ovs_action_encap_nsh *encap_nsh);
