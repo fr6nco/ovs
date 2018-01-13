e81e5a877a3379dc45f397e098cd71ec2cd28c83
Thomas Boros <thomas@Thomass-MacBook-Pro.local>
2018-01-13 21:30:35 +0100

    managing overflow

--- datapath/actions.c
+++ datapath/actions.c
@@ -388,7 +388,11 @@ static int push_eth(struct sk_buff *skb, struct sw_flow_key *key,
 static void inc_field(struct sk_buff *skb, __u32 *seqfield, 
 			uint32_t increment, __sum16 *check)
 {
-	__u32 newval = (__u32) htonl(*seqfield) + (__u32) increment;
+	__u32 seqval = (__u32) htonl(*seqfield);
+	__u32 newval = seqval + (__u32) increment;
+	if (newval < seqval) {
+		newval = seqval - (__u32) increment;
+	}
 	inet_proto_csum_replace4(check, skb, *seqfield, htonl(newval), false);
 	*seqfield = htonl(newval);
 	return;
